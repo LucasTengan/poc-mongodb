3e9481cc7fc18dc8d1597e871047949766916a7e
Lucas Tengan <lucastengan@usp.br>
2022-07-13 01:16:29 -0300

    adicionando swagger para documentação no modelo open-api

--- src/main/java/br/com/lucas/wishlist/WishlistApplication.java
+++ src/main/java/br/com/lucas/wishlist/WishlistApplication.java
@@ -1,9 +1,12 @@
 package br.com.lucas.wishlist;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Wishlist API", version = "2.0", description = "Wishlist Information"))
 public class WishlistApplication {
 
 	public static void main(String[] args) {
